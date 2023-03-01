7799234680555c9a32dc5f35d39f9f2e9bfdd0b5
Monius <m1105930979@gmail.com>
2023-03-01 13:45:13 +0800

    add click to test

---
+++ Test/click.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo docker run --privileged --restart=always -itd \
+    --name warp_socks -e LOG=1 \
+    --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+    --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+    --cap-add NET_ADMIN --cap-add SYS_MODULE \
+    -p 9091:9091 \
+    -v /lib/modules:/lib/modules \
+    monius/docker-warp-socks
