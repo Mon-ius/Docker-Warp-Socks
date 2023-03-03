56fb3f5059ab37782382c02aa08f72545f54dc28
Monius <m1105930979@gmail.com>
2023-03-03 16:34:30 +0800

    add cross-platform test

--- dev/click.sh
+++ dev/click.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo docker run --privileged --restart=always -itd \
+ARCH="linux/amd64" # Expected `linux/amd64``, `linux/arm64`, `linux/arm`, and `linux/s390x`;
+
+if [ -n "$1" ] 
+then
+    ARCH=$1
+fi
+
+sudo docker run --privileged --platform="${ARCH}" --restart=always -itd \
     --name warp_socks -e LOG=1 \
     --sysctl net.ipv6.conf.all.disable_ipv6=0 \
     --sysctl net.ipv4.conf.all.src_valid_mark=1 \
