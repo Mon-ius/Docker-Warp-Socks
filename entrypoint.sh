617cdee431067db8bf8fc8932f5757f84c2ae23a
Monius <m1105930979@gmail.com>
2023-03-03 17:53:51 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@ set -e
 
 sleep 5
 
-IFACE=$(ip route show | grep default | awk '{print $5}')
-IPv4=$(ip -4 address show dev "$IFACE" | awk '/inet/{print $2}' | cut -d/ -f1)
-IPv6=$(ip -6 address show dev "$IFACE" | awk '/inet/{print $2}' | cut -d/ -f1)
+IFACE=$(ip route show default | grep default | awk '{print $5}')
+IPv4=$(ifconfig $IFACE | awk '/inet /{print $2}' | cut -d' ' -f2)
+IPv6=$(ifconfig $IFACE | awk '/inet6 /{print $2}' | cut -d' ' -f2)
 TAR="https://api.github.com/repos/ViRb3/wgcf/releases/latest"
 ARCH=$(dpkg --print-architecture)
 URL=$(curl -fsSL ${TAR} | grep 'browser_download_url' | cut -d'"' -f4 | grep linux | grep "${ARCH}")
 
 if [ ! -e "/opt/wgcf-profile.conf" ]; then
-	curl -LS "${URL}" -o ./wgcf && chmod +x ./wgcf && mv ./wgcf /usr/bin
+	curl -LSs "${URL}" -o ./wgcf && chmod +x ./wgcf && mv ./wgcf /usr/bin
 	wgcf register --accept-tos && wgcf generate && mv wgcf-profile.conf /opt
 	sed -i "/\[Interface\]/a PostDown = ip -6 rule delete from ${IPv6}  lookup main" /opt/wgcf-profile.conf
 	sed -i "/\[Interface\]/a PostUp = ip -6 rule add from ${IPv6} lookup main" /opt/wgcf-profile.conf
