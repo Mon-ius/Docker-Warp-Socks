ae4591f46bc63a60014dc11f68f66b0fe7b85a89
Monius <m1105930979@gmail.com>
2024-02-20 05:43:00 +0800

    fix name incorrect

--- dev/click.sh
+++ dev/click.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 _ARCH="linux/amd64" # Expected `linux/amd64``, `linux/arm64`, `linux/arm`, and `linux/s390x`;
+_NAME="warp_socks"
 _KEY=""
 _SOCK_USER=""
 _SOCK_PWD=""
 _PORT="9091"
 _VER="2"
 
-KEY="${6:-$_KEY}"
-SOCK_PWD="${5:-$SOCK_PWD}"
-SOCK_USER="${4:-$SOCK_USER}"
-PORT="${3:-$_PORT}"
-VER="${2:-$VER}"
-ARCH="${1:-$_ARCH}"
+KEY="${7:-$_KEY}"
+SOCK_PWD="${6:-$SOCK_PWD}"
+SOCK_USER="${5:-$SOCK_USER}"
+PORT="${4:-$_PORT}"
+VER="${3:-$VER}"
+ARCH="${2:-$_ARCH}"
+NAME="${1:-$_NAME}"
 
 sudo docker run --privileged --platform="${ARCH}" --restart=always -itd \
-    --name warp_socks -e LOG=1 \
+    --name "${NAME}" -e LOG=1 \
     -e WGCF_LICENSE_KEY="${KEY}" \
     -e SOCK_USER="${SOCK_USER}" \
     -e SOCK_PWD="${SOCK_PWD}" \
