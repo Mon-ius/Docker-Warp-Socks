1e7d6d0b106c8417fbf36d4de25c0ba581dfb2e2
Monius <m1105930979@gmail.com>
2024-02-21 21:36:21 +0800

    update click

--- dev/click.sh
+++ dev/click.sh
@@ -16,14 +16,42 @@ VER="${3:-$VER}"
 ARCH="${2:-$_ARCH}"
 NAME="${1:-$_NAME}"
 
-sudo docker run --privileged --platform="${ARCH}" --restart=always -itd \
-    --name "${NAME}" -e LOG=1 \
-    -e WGCF_LICENSE_KEY="${KEY}" \
-    -e SOCK_USER="${SOCK_USER}" \
-    -e SOCK_PWD="${SOCK_PWD}" \
-    --sysctl net.ipv6.conf.all.disable_ipv6=0 \
-    --sysctl net.ipv4.conf.all.src_valid_mark=1 \
-    --cap-add NET_ADMIN --cap-add SYS_MODULE \
-    -p "${PORT}":"${PORT}" \
-    -v /lib/modules:/lib/modules \
-    monius/docker-warp-socks
+if [ $1 = "-test" ]; then
+
+    api="$2"
+    json="{
+    \"key\": \"$3\", \
+    \"tos\": \"$4\", \
+    \"locale\": \"$5\", \
+    \"model\": \"$6\", \
+    \"type\": \"$7\" \
+    \"referrer\": \"$8\", \
+    }"
+
+    curl -X POST -fsSL "$api" \
+    -H 'authority: cloudflareclient.com' \
+    -H 'host: api.cloudflareclient.com' \
+    -H 'User-Agent: okhttp/4.12.1' \
+    -H 'accept-encoding: gzip' \
+    -H 'accept-language: en-US,en;q=0.9' \
+    -H 'content-type: application/json; charset=UTF-8' \
+    -H 'connection: Keep-Alive' \
+    -H 'origin: https://cloudflareclient.com' \
+    -H 'referer: https://warp.plus' \
+    -H 'user-agent: okhttp/4.12.1' \
+    --compressed \
+    --data "$json" > /tmp/warp.dat
+
+else
+    sudo docker run --privileged --platform="${ARCH}" --restart=always -itd \
+        --name "${NAME}" -e LOG=1 \
+        -e WGCF_LICENSE_KEY="${KEY}" \
+        -e SOCK_USER="${SOCK_USER}" \
+        -e SOCK_PWD="${SOCK_PWD}" \
+        --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+        --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+        --cap-add NET_ADMIN --cap-add SYS_MODULE \
+        -p "${PORT}":"${PORT}" \
+        -v /lib/modules:/lib/modules \
+        monius/docker-warp-socks
+fi
