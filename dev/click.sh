9886f92defc8417df7f1725a044c17fdf6b9dfa5
Monius <m1105930979@gmail.com>
2024-02-22 19:52:59 +0800

    fix click port

--- dev/click.sh
+++ dev/click.sh
@@ -50,7 +50,7 @@ else
         --sysctl net.ipv6.conf.all.disable_ipv6=0 \
         --sysctl net.ipv4.conf.all.src_valid_mark=1 \
         --cap-add NET_ADMIN --cap-add SYS_MODULE \
-        -p "${PORT}":"${PORT}" \
+        -p "${PORT}":"9091" \
         -v /lib/modules:/lib/modules \
-        monius/docker-warp-socks:"${VER}"
+        "monius/docker-warp-socks:${VER}"
 fi
