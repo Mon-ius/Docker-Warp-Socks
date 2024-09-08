80c3d8df32417c8922dc8b206e8b87f053077728
Monius <m1105930979@gmail.com>
2024-09-09 00:10:40 +0800

    fix v3

--- dev/click.sh
+++ dev/click.sh
@@ -131,5 +131,42 @@ delete_account() {
     echo "Delete Response from server: $RESPONSE"
 }
 
-info=$(get_account)
-echo "$info"
+test() {
+    _ARCH="linux/amd64" # Expected `linux/amd64``, `linux/arm64`, `linux/arm`, and `linux/s390x`;
+    _NAME="warp_socks"
+    _KEY=""
+    _SOCK_USER=""
+    _SOCK_PWD=""
+    _PORT="9091"
+    _VER="v2"
+
+    KEY="${7:-$_KEY}"
+    SOCK_PWD="${6:-$SOCK_PWD}"
+    SOCK_USER="${5:-$SOCK_USER}"
+    PORT="${4:-$_PORT}"
+    VER="${3:-$VER}"
+    ARCH="${2:-$_ARCH}"
+    NAME="${1:-$_NAME}"
+
+    sudo docker run --privileged --platform="${ARCH}" --restart=always -itd \
+        --name "${NAME}" -e LOG=1 \
+        -e WGCF_LICENSE_KEY="${KEY}" \
+        -e SOCK_USER="${SOCK_USER}" \
+        -e SOCK_PWD="${SOCK_PWD}" \
+        --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+        --sysctl net.ipv4.conf.all.src_valid_mark=1 \
+        --cap-add NET_ADMIN --cap-add SYS_MODULE \
+        -p "${PORT}":"9091" \
+        -v /lib/modules:/lib/modules \
+        "monius/docker-warp-socks:${VER}"
+}
+
+if [ $# -eq 0 ]; then
+    info=$(get_account)
+    echo "$info"
+elif [ $# -eq 1 ]; then
+    info=$(get_account "$1")
+    echo "$info"
+else
+    test "$@"
+fi
