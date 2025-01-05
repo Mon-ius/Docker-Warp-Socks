9cdbb80320c121768a996f074a221641e4dd9ae7
Monius <m1105930979@gmail.com>
2025-01-05 23:17:02 +0800

    update v3.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,7 @@ public_key=$(echo "$RESPONSE" | sed -n 's/.*"public_key":"\([^"]*\)".*/\1/p')
 client_hex=$(echo "$RESPONSE" | grep -o '"client_id":"[^"]*' | cut -d'"' -f4 | base64 -d | od -t x1 -An | tr -d ' \n')
 reserved_dec=$(echo "$client_hex" | awk '{printf "[%d, %d, %d]", "0x"substr($0,1,2), "0x"substr($0,3,2), "0x"substr($0,5,2)}')
 
+
 if [ -n "$SOCK_USER" ] && [ -n "$SOCK_PWD" ]; then
     AUTH_PART=$(cat <<EOF
             "users": [
@@ -34,7 +35,26 @@ else
     AUTH_PART=""
 fi
 
-cat <<EOF | tee /etc/sing-box/config.json
+WARP_PART=$(cat <<EOF
+        {
+            "tag": "WARP",
+            "type": "wireguard",
+            "server": "$WARP_SERVER",
+            "server_port": $WARP_PORT,
+            "local_address": [
+                "${ipv4}/32",
+                "${ipv6}/128"
+            ],
+            "private_key": "$private_key",
+            "peer_public_key": "$public_key",
+            "reserved": $reserved_dec,
+            "mtu": 1408,
+            "udp_fragment": true
+        }
+EOF
+)
+
+cat <<EOF | sudo tee /etc/sing-box/config.json
 {
     "log": {
         "disabled": false,
@@ -49,97 +69,11 @@ cat <<EOF | tee /etc/sing-box/config.json
             "store_fakeip": true
         }
     },
-    "dns": {
-        "servers": [
-            {
-                "tag": "google",
-                "address": "tls://8.8.8.8",
-                "detour": "WARP"
-            },
-            {
-                "tag": "fallback",
-                "address": "8.8.8.8",
-                "address_resolver": "google",
-                "detour": "WARP"
-            },
-            {
-                "tag": "local-dns",
-                "address": "223.5.5.5",
-                "detour": "direct"
-            },
-            {
-                "tag": "block-dns",
-                "address": "rcode://success"
-            }
-        ],
-        "rules": [
-            {
-                "outbound": "any",
-                "server": "local-dns"
-            },
-            {
-                "query_type": [
-                    "A"
-                ],
-                "rewrite_ttl": 1,
-                "server": "fallback"
-            }
-        ],
-        "strategy": "ipv4_only"
-    },
-    "route": {
-        "rules": [
-            {
-                "protocol": "dns",
-                "outbound": "dns-out"
-            },
-            {
-                "port": 53,
-                "outbound": "dns-out"
-            },
-            {
-                "type": "logical",
-                "mode": "or",
-                "rules": [
-                    {
-                        "port": 853
-                    },
-                    {
-                        "network": "udp",
-                        "port": 443
-                    },
-                    {
-                        "protocol": "stun"
-                    }
-                ],
-                "outbound": "block"
-            },
-            {
-                "ip_is_private": true,
-                "outbound": "direct"
-            }
-        ],
-        "auto_detect_interface": true,
-        "final": "WARP"
-    },
     "outbounds": [
         {
-            "tag": "WARP",
-            "type": "wireguard",
-            "server": "$WARP_SERVER",
-            "server_port": $WARP_PORT,
-            "local_address": [
-                "${ipv4}/32",
-                "${ipv6}/128"
-            ],
-            "private_key": "$private_key",
-            "peer_public_key": "$public_key",
-            "reserved": $reserved_dec,
-            "mtu": 1408
-        },
-        {
+            "tag": "direct-out",
             "type": "direct",
-            "tag": "direct"
+            "udp_fragment": true
         },
         {
             "type": "dns",
@@ -148,8 +82,57 @@ cat <<EOF | tee /etc/sing-box/config.json
         {
             "type": "block",
             "tag": "block"
-        }
+        },
+$WARP_PART
     ],
+    "dns": {
+        "servers": [
+            {
+                "tag": "ND-h3",
+                "address": "h3://dns.nextdns.io/x",
+                "address_resolver": "dns-direct",
+                "detour": "direct-out"
+            },
+            {
+                "tag": "dns-direct",
+                "address": "udp://223.5.5.5",
+                "detour": "direct-out"
+            }
+        ],
+        "strategy": "ipv4_only",
+        "final": "ND-h3",
+        "reverse_mapping": true,
+        "disable_cache": false,
+        "disable_expire": false
+    },
+    "route": {
+        "rules": [
+            {
+                "protocol": "dns",
+                "outbound": "dns-out"
+            },
+            {
+                "ip_is_private": true,
+                "outbound": "direct-out"
+            },
+            {
+                "ip_cidr": [
+                    "0.0.0.0/8",
+                    "10.0.0.0/8",
+                    "127.0.0.0/8",
+                    "169.254.0.0/16",
+                    "172.16.0.0/12",
+                    "192.168.0.0/16",
+                    "224.0.0.0/4",
+                    "240.0.0.0/4",
+                    "52.80.0.0/16"
+                ],
+                "outbound": "direct-out"
+            }
+        ],
+        "auto_detect_interface": true,
+        "final": "WARP"
+    },
     "inbounds": [
         {
             "type": "mixed",
@@ -157,12 +140,6 @@ cat <<EOF | tee /etc/sing-box/config.json
             "listen": "::",
             "listen_port": $NET_PORT,
 $AUTH_PART
-            "sniff": true
-        },
-        {
-            "type": "direct",
-            "listen": "::",
-            "listen_port": 53,
             "sniff": true
         }
     ]
